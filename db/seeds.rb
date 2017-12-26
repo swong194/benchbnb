3fdae122c86281184fa5c9010199c61dec3876f4
Sunny Wong <swong194@gmail.com>
2017-12-26 13:42:54 -0500

    adds bench-backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+a = Bench.create!(description: 'A nice bench', lat: 37.8, lng:-122.44)
+b = Bench.create!(description: 'A alright bench', lat: 37.5, lng:-122.42)
+c = Bench.create!(description: 'A meh bench', lat: 37.9, lng:-122.44)
